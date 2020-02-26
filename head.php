0ce1b6ed611fe8059126de57ab29d4c656af3ecd
Sanskriti Thakur <53896352+sanskritithakur@users.noreply.github.com>
2020-02-26 20:48:11 +0530

    Add files via upload

---
+++ head.php
@@ -0,0 +1,27 @@
+<?php
+    $base_url = 'http://localhost/project11/'
+?>
+
+<meta charset="utf-8">
+<!-- <meta name="viewport"  content="width=1366" /> -->
+<meta name="description" content="">
+<meta name="author" content="HackerKernel">
+<meta name="keywords" content="">
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!--carousal-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+ <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
+ <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
+ <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+ <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+<link rel="stylesheet" href="<?=$base_url;?>css/main1.css">
+<link rel="stylesheet" href="<?=$base_url;?>css/slider.css">
+<link rel="stylesheet" href="<?=$base_url;?>css/responsive.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap" rel="stylesheet">
+</head>
+
+
+
+<body>
